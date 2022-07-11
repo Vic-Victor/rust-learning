95277946ddcfbd8821780803f630222669317d6e
zhangjunwei@legend.tech <zhangjunwei@legend.tech>
2022-07-11 11:35:14 +0800

    first exercise

---
+++ part1/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+    let x: u32 = 24;
+    println!("The parameter is {}, result of `fibonacci` is: {}", x, fibonacci(x));
+}
+
+/**
+ a fibonacci function sample
+ */
+fn fibonacci(x: u32) -> usize {
+    if x == 0 {
+        return 0;
+    }
+    if x == 1 || x == 2 {
+        return 1;
+    }
+    fibonacci(x - 1) + fibonacci(x -2)
+}
